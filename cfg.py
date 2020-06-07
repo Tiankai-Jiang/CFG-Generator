9ebc4f569d2cbb68a9a557756194fb53d156e560
tiankai <t57jiang@uwaterloo.ca>
2020-06-07 15:58:18 -0400

    added method remove_empty_blocks

--- cfg.py
+++ cfg.py
@@ -1,7 +1,7 @@
 import _ast, ast, astor
 import graphviz as gv
 from __future__ import annotations
-from typing import Dict, List, Tuple, Optional, Type
+from typing import Dict, List, Tuple, Set, Optional, Type
 
 class SingletonMeta(type):
     _instance: Optional[BlockId] = None
@@ -15,7 +15,7 @@ class BlockId(metaclass=SingletonMeta):
 
     counter: int = 0
 
-    def gen(self):
+    def gen(self) -> int:
         self.counter+=1
         return self.counter
 
@@ -27,6 +27,20 @@ class BasicBlock:
         self.calls: List[str] = []
         self.prev: List[int] = []
         self.next: List[int] = []
+
+    def is_empty(self) -> bool:
+        return len(self.stmts) == 0
+
+    def has_next(self) -> bool:
+        return len(self.next) != 0
+
+    def remove_from_prev(self, prev_bid: int) -> None:
+        if prev_bid in self.prev:
+            self.prev.remove(prev_bid)
+
+    def remove_from_next(self, next_bid: int) -> None:
+        if next_bid in self.next:
+            self.next.remove(next_bid)
 
 class CFG:
 
@@ -49,12 +63,11 @@ class CFGVisitor(ast.NodeVisitor):
 
     def build(self, name: str, tree: Type[_ast.AST]) -> CFG:
         self.cfg = CFG(name)
-
         self.curr_block = self.new_block()
         self.cfg.start = self.curr_block
 
         self.visit(tree)
-        # self.clean()
+        # self.remove_empty_blocks()
         return self.cfg
 
     def new_block(self) -> BasicBlock:
@@ -65,11 +78,49 @@ class CFGVisitor(ast.NodeVisitor):
     def add_stmt(self, block: BasicBlock, stmt: Type[_ast.AST]) -> None:
         block.stmts.append(stmt)
 
-    def add_edge(self, frm: BasicBlock, to: BasicBlock, condition=None) -> None:
-        frm.next.append(to.bid)
-        to.prev.append(frm.bid)
-        self.cfg.edges[(frm.bid, to.bid)] = condition
+    def add_edge(self, frm_id: int, to_id: int, condition=None) -> None:
+        self.cfg.blocks[frm_id].next.append(to_id)
+        self.cfg.blocks[to_id].prev.append(frm_id)
+        self.cfg.edges[(frm_id, to_id)] = condition
 
-    
+    def add_loop_block(self) -> BasicBlock:
+        if self.curr_block.is_empty() and not self.curr_block.has_next():
+            return self.curr_block
+        else:
+            loop_block = self.new_block()
+            self.add_edge(self.curr_block.bid, loop_block.bid)
+            return loop_block
+
+    def add_subgraph(self, tree: Type[_ast.AST]) -> None:
+        self.cfg.func_calls[tree.name] = CFGVisitor().build(tree.name, ast.Module(body=tree.body))
+
+    def add_condition(self, cond1: Optional[Type[_ast.AST]], cond2: Optional[Type[_ast.AST]]) -> None:
+        if cond1 and cond2:
+            return ast.BoolOp(ast.And(), values=[cond1, cond2])
+        else:
+            return cond1 if cond1 else cond2
+
+    # not tested
+    def remove_empty_blocks(self, block: BasicBlock, visited: Set[int] = set()) -> None:
+        if block.bid not in visited:
+            visited.add(block.bid)
+            if block.is_empty():
+                for prev_bid in block.prev:
+                    prev_block = self.cfg.blocks[prev_bid]
+                    for next_bid in block.next:
+                        next_block = self.cfg.blocks[next_bid]
+                        self.add_edge(prev_bid, next_bid, self.add_condition(self.cfg.edges[(prev_bid, block.bid)], self.cfg.edges[(block.bid, next_bid)]))
+                        self.cfg.edges.pop((block.bid, next_bid), None)
+                        next_block.remove_from_prev(block.bid)
+                    self.cfg.edges.pop((prev_bid, block.bid), None)
+                    prev_block.remove_from_next(block.bid)
+                block.prev.clear()
+                for next_bid in block.next:
+                    self.remove_empty_blocks(self.cfg.blocks[next_bid], visited)
+                block.next.clear()
+
+            else:
+                for next_bid in block.next:
+                    self.remove_empty_blocks(self.cfg.blocks[next_bid], visited)
 
 
