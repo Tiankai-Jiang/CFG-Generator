56a2b653edd8b1c434dffd2cf75206c690dc619c
Layne Liu <layne.liu@outlook.com>
2022-02-27 16:04:43 +0100

    Refactor visit_FunctionDef

--- examples/01_call.py
+++ examples/01_call.py
@@ -1,16 +1,24 @@
-def func1(x, y = 10):
+def func1(x, y=10):
+    x += 1
     return x + 5
 
 
-def func2(x=1, y =2, z=3, f=4):
+def func2(x=1, y=2, z=3, f=4):
+    x += 1
     return x - 5
 
 
-def fib():
-    a, b = 0, 1
-    while True:
-        yield a
-        a, b = b, a + b
+def fib(n):
+    i = 1
+    s = 0
+    while i <= n:
+        s += i
+        i = i + 1
+    return s
+
 
 a = fib(10)
-b = func2(5)
+if a:
+    b = func2(5)
+else:
+    func1(1)
